d98e80f8684967231386a4ba563650bb42d6e9d9
LizHengsteler <80812851+LizHengsteler@users.noreply.github.com>
2021-04-29 15:08:01 +0200

    Aufgabe 6 Fehler behoben

--- Aufgaben/Aufgabe6/script.ts
+++ Aufgaben/Aufgabe6/script.ts
@@ -1,66 +1,44 @@
-var europa: string = "Europa";
-var afrika: string = "Afrika";
-var südamerika: string = "Südamerika";
-var nordamerika: string = "Nordamerika";
-var asien: string = "Asien";
-var australien: string = "Australien";
+var europe: string = "Europe";
+var africa: string = "Africa";
+var southamerica: string = "Southamerica";
+var northamerica: string = "Northamerica";
+var asia: string = "Asia";
+var australia: string = "Australia";
 
 
 
-var europa18: number = 4209.3;
-var europa08: number = 4965.7;
-var afrika18: number = 1235.5;
-var afrika08: number = 1028;
-var südamerika18: number = 1261.5;
-var südamerika08: number = 1132.6;
-var nordamerika18: number = 6035.6;
-var nordamerika08: number = 6600.4;
-var asien18: number = 16274.1;
-var asien08: number = 12954.7;
-var australien18: number = 2100.5;
-var australien08: number = 1993;
+var europe18: number = 4209.3;
+var europe08: number = 4965.7;
+var africa18: number = 1235.5;
+var africa08: number = 1028;
+var southamerica18: number = 1261.5;
+var southamerica08: number = 1132.6;
+var northamerica18: number = 6035.6;
+var northamerica08: number = 6600.4;
+var asia18: number = 16274.1;
+var asia08: number = 12954.7;
+var australia18: number = 2100.5;
+var australia08: number = 1993;
 
-var world : number = europa18 + afrika18 + südamerika18 + nordamerika18 + asien18 + australien18;
-var relativeuropa : number = Math.round(europa18 / world * 100 * 100) / 100;
-var relativafrika : number =  Math.round(afrika18 / world * 100 * 100) / 100;
-var relativsüdamerika : number = Math.round(südamerika18 / world * 100 * 100) / 100;
-var relativnordamerika : number = Math.round(nordamerika18 / world * 100 * 100) / 100;
-var relativasien : number = Math.round(asien18 / world * 100 * 100) / 100;
-var relativaustralien : number = Math.round(australien18 / world * 100 * 100) / 100;
-
-var differenzeuropa : number = Math.round((europa18 - europa08) / europa08 * 100 * 100) / 100;
-var differenzafrika : number = Math.round((afrika18 - afrika08) / afrika08 * 100 * 100) / 100;
-var differenzsüdamerika : number = Math.round((südamerika18 - südamerika08) / südamerika08 * 100 * 100) / 100;
-var differenznordamerika : number = Math.round((nordamerika18 - nordamerika08) / nordamerika08 * 100 * 100) / 100;
-var differenzasien : number = Math.round((asien18 - asien08) / asien08 * 100 * 100) / 100;
-var differenzaustralien : number = Math.round((australien18 - australien08) / australien08 * 100 * 100) / 100;
-
-var emissioneuropa : number = europa18 - europa08;
-var emissionafrika : number = afrika18 - afrika08;
-var emissionsüdamerika : number = südamerika18 - südamerika08;
-var emissionnordamerika : number= nordamerika18 - nordamerika08; 
-var emissionasien : number = asien18 - asien08;
-var emissionaustralien : number = australien18 - australien08;
+var world : number = europe18 + africa18 + southamerica18 + northamerica18 + asia18 + australia18;
 
 
+window.addEventListener("load", function() {
+
+    document.querySelector(".europe").addEventListener("click", function() { emission (europe, europe18, europe08);});
+    document.querySelector(".northamerica").addEventListener("click" , function() { emission(northamerica, northamerica18, northamerica08);});
+    document.querySelector(".southamerica").addEventListener("click" , function() { emission(southamerica, southamerica18, southamerica08)})
+    document.querySelector(".africa").addEventListener("click" , function() { emission(africa, africa18, africa08 )})
+    document.querySelector(".asia").addEventListener("click" , function() { emission(asia, asia18, asia08  )})
+    document.querySelector(".australia").addEventListener("click" , function() { emission(australia, australia18, australia08 )})
 
 
-
-window.addEventListener("load", function(){
-
-    document.querySelector(".europe").addEventListener("click" , function() { emission(europa, europa18, europa08 )})
-    document.querySelector(".northamerica").addEventListener("click" , function() { emission(nordamerika, nordamerika18, nordamerika08)})
-    document.querySelector(".southamerica").addEventListener("click" , function() { emission(südamerika, südamerika18, südamerika08)})
-    document.querySelector(".africa").addEventListener("click" , function() { emission(afrika, afrika18, afrika08 )})
-    document.querySelector(".asia").addEventListener("click" , function() { emission(asien, asien18, asien08  )})
-    document.querySelector(".northamerica").addEventListener("click" , function() { emission(nordamerika, nordamerika18, nordamerika08 )})
-
-    function emission (continentName : string , emission18 : number, emission08 : number) {
-        document.querySelector (".titleRegion").innerHTML = continentName;
-        document.querySelector (".gesamtEmission18").innerHTML = emission18.toString();
-        
-
-
+    function emission (continentName : string, emission18 : number, emission08 : number) {
+        document.querySelector (".continent").innerHTML = continentName;
+        document.querySelector (".emissions18").innerHTML = emission18.toString();
+        document.querySelector(".relativeWorld").innerHTML = Math.round(emission18 / world * 100 * 100) / 100 + "%";
+        document.querySelector(".growthRate").innerHTML = Math.round((emission18 - emission08) / emission08 * 100 * 100 / 100) + "%";
+        document.querySelector(".growthRateAbsolute").innerHTML = (Math.round((emission18 - emission08) * 100) / 100); 
     };
     
 
