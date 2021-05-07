3a447f6967b71f7d6b0c8795c81c8db82f5cbec5
LizHengsteler <80812851+LizHengsteler@users.noreply.github.com>
2021-05-07 11:19:28 +0200

    Aufgabe 7

---
+++ Aufgaben/Aufgabe7/script7.ts
@@ -0,0 +1,40 @@
+
+
+
+window.addEventListener("load", function(){
+
+    
+
+    document.querySelector(".button1").addEventListener("click" , function() { beats ("Sounds/kick.mp3")});
+    document.querySelector(".button2").addEventListener("click" , function() { beats ("Sounds/hihat.mp3")});
+    document.querySelector(".button3").addEventListener("click" , function() { beats ("Sounds/snare.mp3")});
+    document.querySelector(".button4").addEventListener("click" , function() { beats ("Sounds/A.mp3")});
+    document.querySelector(".button5").addEventListener("click" , function() { beats ("Sounds/C.mp3")});
+    document.querySelector(".button6").addEventListener("click" , function() { beats ("Sounds/F.mp3")});
+    document.querySelector(".button7").addEventListener("click" , function() { beats ("Sounds/G.mp3")});
+    document.querySelector(".button8").addEventListener("click" , function() { beats ("Sounds/laugh-1.mp3")});
+    document.querySelector(".button9").addEventListener("click" , function() { beats ("Sounds/laugh-2.mp3")});
+
+
+        function beats (music) {
+            var sound :HTMLAudioElement = new Audio(music); 
+            sound.play();
+            
+    };
+
+    var sounds: HTMLAudioElement[] = [new Audio ("sounds/kick.mp3"), new Audio ("sounds/hihat.mp3"), new Audio ("sounds/snare.mp3"),]
+    sounds.play();
+    var sounds = 0;
+    setInterval(function() playbutton { 
+
+        sounds = sounds + 1;
+        console.log(sounds)
+        
+    
+    }, 500);
+    
+
+
+    
+
+});
