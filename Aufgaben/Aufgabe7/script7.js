254fdf60fd6fe1cad08c6edf2de71595f0657158
LizHengsteler <80812851+LizHengsteler@users.noreply.github.com>
2021-05-10 11:27:25 +0200

    Aufgabe 7 Verbesserung

--- Aufgaben/Aufgabe7/script7.js
+++ Aufgaben/Aufgabe7/script7.js
@@ -1,19 +1,28 @@
 window.addEventListener("load", function () {
-    document.querySelector(".button1").addEventListener("click", function () { playSample("sounds/kick.mp3"); });
-    document.querySelector(".button2").addEventListener("click", function () { playSample("sounds/hihat.mp3"); });
-    document.querySelector(".button3").addEventListener("click", function () { playSample("sounds/snare.mp3"); });
-    document.querySelector(".button4").addEventListener("click", function () { playSample("sounds/A.mp3"); });
-    document.querySelector(".button5").addEventListener("click", function () { playSample("sounds/C.mp3"); });
-    document.querySelector(".button6").addEventListener("click", function () { playSample("sounds/F.mp3"); });
-    document.querySelector(".button7").addEventListener("click", function () { playSample("sounds/G.mp3"); });
-    document.querySelector(".button8").addEventListener("click", function () { playSample("sounds/laugh-1.mp3"); });
-    document.querySelector(".button9").addEventListener("click", function () { playSample("sounds/laugh-2.mp3"); });
+    var sounds = [];
+    sounds[0] = new Audio("sounds/kick.mp3");
+    sounds[1] = new Audio("sounds/hihat.mp3");
+    sounds[2] = new Audio("sounds/snare.mp3");
+    sounds[3] = new Audio("sounds/A.mp3");
+    sounds[4] = new Audio("sounds/C.mp3");
+    sounds[5] = new Audio("sounds/F.mp3");
+    sounds[6] = new Audio("sounds/G.mp3");
+    sounds[7] = new Audio("sounds/laugh-1.mp3");
+    sounds[8] = new Audio("sounds/laugh-2.mp3");
+    document.querySelector(".button1").addEventListener("click", function () { playSample(sounds[0].play()); });
+    document.querySelector(".button2").addEventListener("click", function () { playSample(sounds[1].play()); });
+    document.querySelector(".button3").addEventListener("click", function () { playSample(sounds[2].play()); });
+    document.querySelector(".button4").addEventListener("click", function () { playSample(sounds[3].play()); });
+    document.querySelector(".button5").addEventListener("click", function () { playSample(sounds[4].play()); });
+    document.querySelector(".button6").addEventListener("click", function () { playSample(sounds[5].play()); });
+    document.querySelector(".button7").addEventListener("click", function () { playSample(sounds[6].play()); });
+    document.querySelector(".button8").addEventListener("click", function () { playSample(sounds[7].play()); });
+    document.querySelector(".button9").addEventListener("click", function () { playSample(sounds[8].play()); });
     function playSample(music) {
-        var sound = new Audio(music);
-        sound.play();
+        var sounds = new Audio(music);
+        sounds.play();
     }
     ;
-    var sounds = [new Audio("sounds/kick.mp3"), new Audio("sounds/hihat.mp3"), new Audio("sounds/snare.mp3"), new Audio("sounds/A.mp3"), new Audio("sounds/C.mp3"), new Audio("sounds/F.mp3"), new Audio("sounds/G.mp3"), new Audio("sounds/laugh-1.mp3"), new Audio("sounds/laugh-2.mp3")];
     function intervall() {
         setTimeout(function () {
             sounds[0].play();
